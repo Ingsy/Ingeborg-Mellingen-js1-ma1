0e87bc5b092387823d78ebaaea907d11e381d7b7
Ingsy <ingeborg_mellingen89@hotmail.com>
2021-06-15 11:27:55 +0200

    test

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+const cat = {
+      complain: function() {console.log ("Meow!");
+    }
+    };
+
+
+
+
+const cats = [
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
+];
+
+function changeHeader() {
+  h3Element.innerHTML = "heading";
+  document.title = "Updated heading";
+  h3Element.style.fontSize = "2em";
+}
+
+var H3 = document.querySelector(".heading");
+
+var (HTMLHeadingElement)
+
+document.getElementsByClassName("Subheading").innerHTML = totalPrice;
+
